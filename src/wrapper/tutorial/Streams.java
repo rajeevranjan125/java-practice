06764e835b05425bb24b7745cc00c9411b4cf66d
Rajeev Ranjan Prasad <rajeevranjan125@users.noreply.github.com>
2026-05-03 02:22:28 +0530

    learned things

--- src/wrapper/tutorial/Streams.java
+++ src/wrapper/tutorial/Streams.java
@@ -3,16 +3,49 @@ package wrapper.tutorial;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Streams {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7));
 
-       List<Integer> ans = list.stream()
-                .filter(x -> x % 2 == 0)
-                .map(x -> x * x)
-                .sorted()
+        List<Integer> list = new ArrayList<>(Arrays.asList(10, 10, 2, 3, 4, 5, 6, 7));
+
+
+        List<Integer> ans = list.stream()
+                .distinct()
                 .toList();
-        System.out.println(ans);
+
+//      Stream<Integer> stream=  Stream.of(1,2,3);
+
+//      stream.sorted((a,b)->b-a).forEach(System.out::println);
+
+//        System.out.println(ans);
+//        List<Integer> ans = list.stream()
+//                .filter((x) -> x % 2 == 0)
+//                .map(x -> x * 2)
+//                .map(x -> x * 5)
+//                .sorted()
+//                .distinct()
+//                .limit(2)
+//                .collect(Collectors.toList());
+
+//      Stream<Integer> stream =  Stream.iterate(10,x->x+2);
+//
+//        System.out.println(stream.limit(10).toList());
+
+//        Stream<Double> streams = Stream.generate(Math::random).map(x -> x * 10000).limit(10);
+//
+//        streams.forEach(System.out::println);
+
+//        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+//
+//        numbers.stream()
+//                .peek(n -> System.out.println("Original: " + n))
+//                .map(n -> n * 2)
+//                .peek(n -> System.out.println("After map: " + n))
+//                .forEach(System.out::println);
+
+
     }
 }
